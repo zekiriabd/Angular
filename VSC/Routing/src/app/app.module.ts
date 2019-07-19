a8fbbaecaba31bde703935b2359b938486784e7d
zekiriabd <zekiriabd@yahoo.fr>
2019-07-19 09:10:50 -0700

    routing

---
+++ VSC/Routing/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    SecondComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'page1', component: FirstComponent },
+      { path: 'page2', component: SecondComponent },
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
